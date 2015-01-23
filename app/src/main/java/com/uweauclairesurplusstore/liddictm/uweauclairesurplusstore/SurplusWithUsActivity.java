983cb8d2b919436060b61edae96147a05e3f83ea
liddictm <liddictm@uwec.edu>
2015-01-23 11:37:46 -0600

    Updated the Surplus W/ Us page. Got it looking good, now moving onto the Rules page by updating layout and adding some code.

--- app/src/main/java/com/uweauclairesurplusstore/liddictm/uweauclairesurplusstore/SurplusWithUsActivity.java
+++ app/src/main/java/com/uweauclairesurplusstore/liddictm/uweauclairesurplusstore/SurplusWithUsActivity.java
@@ -1,25 +1,119 @@
 package com.uweauclairesurplusstore.liddictm.uweauclairesurplusstore;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 
-public class SurplusWithUsActivity extends ActionBarActivity {
+public class SurplusWithUsActivity extends ActionBarActivity implements NavigationDrawerFragment.NavigationDrawerCallbacks {
+    /**
+     * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
+     */
+    private NavigationDrawerFragment mNavigationDrawerFragment;
+
+    /**
+     * Used to store the last screen title. For use in {@link #restoreActionBar()}.
+     */
+    private CharSequence mTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_surplus_with_us);
+
+        mNavigationDrawerFragment = (NavigationDrawerFragment)
+                getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
+        mTitle = getTitle();
+
+        // Set up the drawer.
+        mNavigationDrawerFragment.setUp(
+                R.id.navigation_drawer,
+                (DrawerLayout) findViewById(R.id.drawer_layout));
+    }
+
+    public void aboutUsOnClick(View v) {
+        Button aboutUs = (Button) v;
+        startActivity(new Intent(getApplicationContext(), AboutUsActivity.class));
+    }
+
+    public void surplusWithUsOnClick(View v) {
+        Button surplusWithUs = (Button) v;
+        startActivity(new Intent(getApplicationContext(), SurplusWithUsActivity.class));
+    }
+
+    public void rulesOnClick(View v) {
+        Button rules = (Button) v;
+        startActivity(new Intent(getApplicationContext(), RulesActivity.class));
+    }
+
+    public void salesDatesOnClick(View v) {
+        Button salesDates = (Button) v;
+        startActivity(new Intent(getApplicationContext(), SalesDatesActivity.class));
+    }
+
+    public void directionsOnClick(View v) {
+        Button directions = (Button) v;
+        startActivity(new Intent(getApplicationContext(), DirectionsActivity.class));
+    }
+
+    public void contactUsOnClick(View v) {
+        Button contactUs = (Button) v;
+        startActivity(new Intent(getApplicationContext(), ContactUsActivity.class));
+    }
+
+    @Override
+    public void onNavigationDrawerItemSelected(int position) {
+        // update the main content by replacing fragments
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.Background, PlaceholderFragment.newInstance(position + 1))
+                .commit();
+    }
+
+ /*   public void onSectionAttached(int number) {
+        switch (number) {
+            case 1:
+                mTitle = getString(R.string.title_section1);
+                break;
+            case 2:
+                mTitle = getString(R.string.title_section2);
+                break;
+            case 3:
+                mTitle = getString(R.string.title_section3);
+                break;
+        }
+    }
+    */
+
+    public void restoreActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+        actionBar.setDisplayShowTitleEnabled(true);
+        actionBar.setTitle(mTitle);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_surplus_with_us, menu);
-        return true;
+        if (!mNavigationDrawerFragment.isDrawerOpen()) {
+            // Only show items in the action bar relevant to this screen
+            // if the drawer is not showing. Otherwise, let the drawer
+            // decide what to show in the action bar.
+            getMenuInflater().inflate(R.menu.main, menu);
+            restoreActionBar();
+            return true;
+        }
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -36,4 +130,48 @@ public class SurplusWithUsActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+
+     /*   @Override
+        public void onAttach(Activity activity) {
+            super.onAttach(activity);
+            ((MainActivity) activity).onSectionAttached(
+                    getArguments().getInt(ARG_SECTION_NUMBER));
+        }
+    }
+    */
+
+    }
 }
+
